4b179cf247608d28c39a407f44b1eacab86d3944
yoychen <yui12327@gmail.com>
2020-06-12 13:26:23 +0000

    Remove unused files from create-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
 import React from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { hot } from "react-hot-loader/root";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default process.env.NODE_ENV === "development" ? hot(App) : App;
