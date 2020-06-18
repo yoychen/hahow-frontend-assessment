86216a0188d756c67f05dd537c5ce6203b3019ec
yoychen <yui12327@gmail.com>
2020-06-18 17:35:51 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.664617a85443e35f3b183b952bdb8a6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc0f4ca2c8ec210e27130e8de6053a5",
+    "url": "/hahow-frontend-assessment/index.html"
+  },
+  {
+    "revision": "e4777e44659e92205128",
+    "url": "/hahow-frontend-assessment/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ea35ecd3006d0097c895",
+    "url": "/hahow-frontend-assessment/static/css/main.9358cf48.chunk.css"
+  },
+  {
+    "revision": "e4777e44659e92205128",
+    "url": "/hahow-frontend-assessment/static/js/2.b206e8eb.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/hahow-frontend-assessment/static/js/2.b206e8eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea35ecd3006d0097c895",
+    "url": "/hahow-frontend-assessment/static/js/main.ac4b5cd2.chunk.js"
+  },
+  {
+    "revision": "77a1d8980ba11b47512d",
+    "url": "/hahow-frontend-assessment/static/js/runtime-main.cc27e9ae.js"
+  }
+]);
